d0792531f7dbbc50b2cf971dfa18399f3112a667
Marynryn <murmar95@gmail.com>
2024-10-15 10:55:19 +0300

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import ThemeButton from '@/components/common/themeButton';
 import { ContactForm } from '@/components/common/contact-form/contact-form';
 import { Logo } from '@/components/ui/logo/logo';
 import { Navigation } from '@/components/ui/navigation/navigation';
+import { AboutInfo } from '@/components/common/about-info/aboutInfo';
+import { About } from '@/sections/About/About';
 
 export default function Home() {
   return (
     <main>
+      <About />
       <div className="container">
         <Logo className="mt-5" />
         <Navigation className="mt-5" />
@@ -15,6 +18,7 @@ export default function Home() {
         <button type="button" className="yellow-button ">
           Переглянути каталог
         </button>
+
         <ContactForm />
       </div>
     </main>
