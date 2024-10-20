e45bbaab3e6121fca2f70660f4b6d653c240eded
Aleksandr2905 <natalukha@ukr.net>
2024-10-20 18:01:08 +0300

    added isActive in navigation

--- src/components/ui/navigation/navigation.tsx
+++ src/components/ui/navigation/navigation.tsx
@@ -1,23 +1,35 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { NavProps } from './types';
 import common from '@/data/common.json';
+import clsx from 'clsx';
 
 export const Navigation = ({ className }: NavProps) => {
+  const pathname = usePathname();
   const { navigates } = common;
 
   return (
     <nav className={className}>
       <ul className="flex flex-col gap-[10px] xl:flex-row">
-        {navigates.map((i) => (
-          <li key={i.id}>
-            <Link
-              href={i.href}
-              className="min-w-[150px] inline-flex items-center justify-center py-4 px-10 rounded-[30px] border border-text60Lt border-opacity-40 dark:border-text60Dt animation text-sm leading-[1.28] font-medium -tracking-[0.42px] hover:border-accentLt hover:text-accentLt hover:dark:border-accentDt hover:dark:text-accentDt dark:hover:border-accentDt dark:hover:text-accentDt md:text-base md:leading-5 md:-tracking-[0.48px]"
-            >
-              {i.name}
-            </Link>
-          </li>
-        ))}
+        {navigates.map((i) => {
+          const isActive = pathname === i.href;
+          return (
+            <li key={i.id}>
+              <Link
+                href={i.href}
+                className={clsx(
+                  'min-w-[150px] inline-flex items-center justify-center py-4 px-10 rounded-[30px] border border-text60Lt border-opacity-40 dark:border-text60Dt animation text-sm leading-[1.28] font-medium -tracking-[0.42px] hover:border-accentLt hover:text-accentLt hover:dark:border-accentDt hover:dark:text-accentDt md:text-base md:leading-5 md:-tracking-[0.48px]',
+                  isActive &&
+                    'border-hoverLt bg-additionalLt text-hoverLt dark:border-hoverDt dark:bg-additionalDt dark:text-hoverDt'
+                )}
+              >
+                {i.name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
